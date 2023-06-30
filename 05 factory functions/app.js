7a920ebb19a5a803af1ee248f366b36a16930178
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-30 09:50:48 -0400

    add factory functions lesson complete

--- 05 factory functions/app.js
+++ 05 factory functions/app.js
@@ -1,19 +1,29 @@
-//
-const john = {
-  firstName: 'Mr',
-  lastName: 'anderson',
-  fullNmae: function () {
-    console.log(`my full name is ${this.firstName} ${this.lastName}`);
-  },
-};
+// //
+// const john = {
+//   firstName: 'Mr',
+//   lastName: 'anderson',
+//   fullName: function () {
+//     console.log(`my full name is ${this.firstName} ${this.lastName}`);
+//   },
+// };
 
-const bob = {
-  firstName: 'ana',
-  lastName: 'sanders',
-  fullNmae: function () {
-    console.log(`my full name is ${this.firstName} ${this.lastName}`);
-  },
-};
+// const bob = {
+//   firstName: 'ana',
+//   lastName: 'sanders',
+//   fullName: function () {
+//     console.log(`my full name is ${this.firstName} ${this.lastName}`);
+//   },
+// };
 
-john.fullNmae();
-bob.fullNmae();
+function createPerson(firstName, lastName) {
+  return {
+    firstName,
+    lastName,
+    fullName: function () {
+      console.log(`my full name is ${this.firstName} ${this.lastName}`);
+    },
+  };
+}
+
+const john = createPerson('polo', 'ralph');
+john.fullName();
