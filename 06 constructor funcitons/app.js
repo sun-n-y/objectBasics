8f84318ba09f0ee690c9397eac4f20f0300f359a
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-30 10:07:19 -0400

    add constructor functions lesson complete

--- 06 constructor funcitons/app.js
+++ 06 constructor funcitons/app.js
@@ -1,5 +1,21 @@
 //
 
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.fullName = function () {
+    console.log(
+      `my full name is ${this.firstName} ${this.lastName} i love react`
+    );
+  };
+  console.log(this);
+}
+
+const john = new Person('jo', 'mo');
+console.log(john.fullName());
+const toll = new Person('df', 'dfg');
+console.log(toll.fullName());
+
 function createPerson(firstName, lastName) {
   return {
     firstName,
