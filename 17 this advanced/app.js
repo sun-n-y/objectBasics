a22f4485706e9509d7191b8558b22aa9fed1fd47
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-01 18:20:50 -0400

    add this advanced lesson complete

---
+++ 17 this advanced/app.js
@@ -0,0 +1,31 @@
+//
+function showThis() {
+  console.log(this);
+}
+
+const john = {
+  name: 'john',
+  showThis: showThis,
+};
+
+const bob = {
+  name: 'bob',
+  showThis: showThis,
+};
+
+// john.showThis();
+// bob.showThis();
+
+// showThis();
+
+const btn1 = document.querySelector('.btn-1');
+const btn2 = document.querySelector('.btn-2');
+
+btn1.addEventListener('click', showThis);
+btn2.addEventListener('click', showThis);
+btn2.addEventListener('click', function () {
+  showThis();
+});
+btn2.addEventListener('click', function () {
+  console.log(this);
+});
