10226c79f3e5fc547bf2a25caed517b4c0c32828
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-02 12:11:56 -0400

    add constructor property complete

--- 20 constructor property/app.js
+++ 20 constructor property/app.js
@@ -7,5 +7,21 @@ function Person(firstName, lastName) {
         `my full name is ${this.firstName} ${this.lastName} and i love react`
       );
     });
-  console.log(this);
 }
+
+const john = new Person('john', 'sanders');
+//to see the constructor that created it
+//console.log(john.constructor);
+
+const tom = {};
+console.log(tom.constructor);
+
+const list = [];
+console.log(list.constructor);
+
+const fx = function () {};
+console.log(fx.constructor);
+
+const susy = new john.constructor('fdfg', 'sdf');
+console.log(susy);
+susy.fullName();
