fa84107e667ef7f121950265ec896e6a58b3dec0
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-02 15:52:10 -0400

    add bind complete

--- 25 bind/app.js
+++ 25 bind/app.js
@@ -15,10 +15,7 @@ function greet(city, country) {
   );
 }
 
-// greet.call(louis, 'sandeigo', 'us');
-// greet.call(tom, 'colorado', 'us');
-// greet.call({ name: 'peter', age: 30 }, 'spain', 'madrid');
+//assign, call it later
+const tomGreet = greet.bind(tom, 'usa', 'usa');
 
-greet.apply(louis, ['red', 'yellow']);
-greet.apply(tom, ['red', 'yellow']);
-greet.apply({ name: 'peter', age: 30 }, ['red', 'yellow']);
+tomGreet();
