dbfaf1c2a3c69d31199808e1054beb9a01ece580
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-02 15:46:35 -0400

    add apply complete

--- 24 apply/app.js
+++ 24 apply/app.js
@@ -1,10 +1,6 @@
 const tom = {
   name: 'tom',
   age: 25,
-  greet() {
-    console.log(this);
-    console.log(`hello, i am ${this.name} and i am ${this.age} years old`);
-  },
 };
 
 const louis = {
@@ -12,14 +8,17 @@ const louis = {
   age: 21,
 };
 
-// function greet() {
-//   console.log(this);
-//   console.log(`hello, i am ${this.name} and i am ${this.age} years old`);
-// }
+function greet(city, country) {
+  console.log(this);
+  console.log(
+    `hello, i am ${this.name} and i am ${this.age} years old and i live i ${city} of ${country}`
+  );
+}
 
-// greet.call(louis);
-// greet.call(tom);
+// greet.call(louis, 'sandeigo', 'us');
+// greet.call(tom, 'colorado', 'us');
+// greet.call({ name: 'peter', age: 30 }, 'spain', 'madrid');
 
-// greet.call({ name: 'peter', age: 30 });
-
-tom.greet.call(louis);
+greet.apply(louis, ['red', 'yellow']);
+greet.apply(tom, ['red', 'yellow']);
+greet.apply({ name: 'peter', age: 30 }, ['red', 'yellow']);
